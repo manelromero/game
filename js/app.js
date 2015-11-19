f2ad18778409c1a58b1a43354cd0bbb9a0514107
Manel Romero <manel@manelromero.com>
2015-11-19 01:15:31 +0100

    Add comments to app.js

--- js/app.js
+++ js/app.js
@@ -4,67 +4,54 @@ var Enemy = function(x, y) {
 	this.y = y;
 	this.speed = Math.floor(Math.random() * (300 - 160)) + 160;
 	this.sprite = 'images/enemy-bug.png';
-
-	// Variables applied to each of our instances go here,
-	// we've provided one for you to get started
-
-	// The image/sprite for our enemies, this uses
-	// a helper we've provided to easily load images
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position
 Enemy.prototype.update = function(dt) {
-	// You should multiply any movement by the dt parameter
-	// which will ensure the game runs at the same speed for
-	// all computers.
+	// Multiply horizontal speed by dt parameter
 	this.x += this.speed * dt;
+	// when an enemy reaches the end of the screen
 	if (this.x > 510) deleteEnemy(this);
 };
-
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
 var Player = function(x, y) {
 	this.x = x;
 	this.y = y;
 	this.sprite = 'images/char-boy.png';
 };
-
+// Update the enemy's position
 Player.prototype.update = function(dt) {};
-
+// Draw the player on the screen
 Player.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
+// Update the enemy's position with the arrow keys
 Player.prototype.handleInput = function(key) {
+	var xVar = 0;
+	var yVar = 0;
 	switch(key) {
 	case 'left':
-		this.x <= 0 ? this.x = 0 : this.x -= 100;
+		if(this.x > 0) this.x -= 100;
 		break;
 	case 'up':
 		this.y <= 44 ? this.y = 380 : this.y -= 84;
 		break;
 	case 'right':
-		this.x >= 400 ? 400 : this.x += 100;
+		if(this.x < 400) this.x += 100;
 		break;
 	case 'down':
-		this.y >= 380 ? 380 : this.y += 84;
+		if(this.y < 380) this.y += 84;
 		break;
 	}
 }
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Enemies array
 var allEnemies = [];
-
+// Create x number of enemies function
 function createEnemy(x) {
 	for(var i=0; i<x; i++) {
 		var row = Math.floor(Math.random() * 3);
@@ -73,7 +60,7 @@ function createEnemy(x) {
 		allEnemies.push(enemy);
 	}
 }
-
+// Delete an Enemy object from the array and create another one
 function deleteEnemy(enemy) {
 	for(var i=0; i<allEnemies.length; i++) {
 		if(allEnemies[i] === enemy) {
@@ -82,7 +69,7 @@ function deleteEnemy(enemy) {
     }
   }
 }
-
+// Finally create player and enemies
 createEnemy(4);
 var player = new Player(200, 380);
 
